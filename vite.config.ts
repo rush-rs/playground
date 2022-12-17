b540b280b256524f6337132e7107de4d249d29fa
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-12-17 11:50:05 +0100

    Use manual chunking in Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+    plugins: [svelte()],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: (id: any) => {
+                    if (id.includes('node_modules')) {
+                        if (id.includes('@smui') || id.includes('@material')) {
+                            return 'vendor_mui'
+                        } else if (id.includes('@lezer') || id.includes('@codemirror')) {
+                            return 'vendor_codemirror'
+                        }
+                        return 'vendor'
+                    }
+                },
+            },
+        },
+    },
 })
